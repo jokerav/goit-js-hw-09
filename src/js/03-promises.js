b81a41c7e9e64db2a08e744b6e725b2c96656911
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-13 22:06:28 +0200

    Ссылки на елементы

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,14 @@
+const btnCreatePromise = document.querySelector('button[type="submit"]');
+const inputFirstDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+console.log(inputAmount);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+        // Fulfill
+    } else {
+        // Reject
+    }
 }
