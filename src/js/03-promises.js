fdc292c0e6b48058a3f051b8249862eb0e72efff
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-13 22:52:01 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,13 +2,26 @@ const btnCreatePromise = document.querySelector('button[type="submit"]');
 const inputFirstDelay = document.querySelector('input[name="delay"]');
 const inputStep = document.querySelector('input[name="step"]');
 const inputAmount = document.querySelector('input[name="amount"]');
-console.log(inputAmount);
+btnCreatePromise.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    const firstDelay = inputFirstDelay.value;
+    const step = inputStep.value;
+    const amount = inputAmount.value;
+    console.log(firstDelay, step, amount);
+    createPromise(1, firstDelay);
+}
 
 function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-        // Fulfill
-    } else {
-        // Reject
-    }
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                resolve('Success! Value passed to resolve function');
+            } else {
+                reject('Error! Error passed to reject function');
+            }
+        }, delay);
+    });
 }
